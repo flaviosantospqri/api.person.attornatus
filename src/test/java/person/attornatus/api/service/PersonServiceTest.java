41ca99526ea575638d777a09d53fb1e35ea653e0
Flavio Santos <flavio.pasantos@outlook.com>
2023-01-17 00:29:41 -0300

    Fix create initial test's for methods controller

--- src/test/java/person/attornatus/api/service/PersonServiceTest.java
+++ src/test/java/person/attornatus/api/service/PersonServiceTest.java
@@ -3,16 +3,15 @@ package person.attornatus.api.service;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import person.attornatus.api.dto.request.PersonRequestDTO;
 import person.attornatus.api.model.Person;
 import person.attornatus.api.repository.PersonRepository;
-
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
-
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
@@ -38,6 +37,39 @@ public class PersonServiceTest {
 
     }
 
+    @Test
+    public void findByExternalUUID_shouldReturnPerson_withValidUUID(){
+        Person personComplete = getMockPersonComplete();
+
+        when(personRepository.findPersonByExternalUUID(personComplete.getExternalUUID())).thenReturn(Optional.of(personComplete));
+
+        Person foundedPerson = personService().findByExternalUUID(personComplete.getExternalUUID());
+
+        Assertions.assertEquals(personComplete.getName(), foundedPerson.getName());
+    }
+
+    @Test
+    public void findAllPerson_shouldReturnAllPerson(){
+        Person personComplete = getMockPersonComplete();
+
+        when(personRepository.findAll()).thenReturn(List.of(personComplete));
+
+        List<Person> persons = personService().findAllPerson();
+
+        Assertions.assertNotNull(persons);
+
+        Assertions.assertEquals(1, persons.size());
+
+    }
+
+    private PersonRequestDTO getPersonDTOMock(){
+        PersonRequestDTO personTest = new PersonRequestDTO();
+
+        personTest.setName("John");
+        personTest.setBirthDate(LocalDate.now().minusDays(10));
+
+        return personTest;
+    }
     private Person getMockPersonRequest(){
         Person personTest = new Person();
 
